6fc3c4d72257d87eb6ea2950f10e71d6758911c6
Ngo Quang Cuong <bestearnmoney87@gmail.com>
2018-09-26 02:48:31 +0700

    initial commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,27 @@
+/**
+ * GiaPhuGroup Co., Ltd.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GiaPhuGroup.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.giaphugroup.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    PHPCuong
+ * @package     PHPCuong_PaymentTerms
+ * @copyright   Copyright (c) 2018-2019 GiaPhuGroup Co., Ltd. All rights reserved. (http://www.giaphugroup.com/)
+ * @license     https://www.giaphugroup.com/LICENSE.txt
+ */
+var config = {
+    // These codes helps us to override the knockout HTML template file.
+    map: {
+        '*': {
+            'Magento_CheckoutAgreements/template/checkout/checkout-agreements.html': 'PHPCuong_PaymentTerms/template/checkout/checkout-agreements.html'
+        }
+    }
+};
